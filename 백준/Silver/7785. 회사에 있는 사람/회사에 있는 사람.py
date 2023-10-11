3d6d77cd997614476903d11b16f8fdd1ed847006
Yoonho Cho <124619604+dev-yoonho@users.noreply.github.com>
2023-10-11 14:34:59 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 236 ms, Memory: 48332 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+temp = dict()
+for _ in range(n):
+     a, b = map(str, sys.stdin.readline().rstrip().split())
+     if b == "enter":
+         temp[a] =b
+     else:
+         del temp[a]
+
+temp = sorted(temp.keys(), reverse = True)
+for i in temp:
+    print(i)
