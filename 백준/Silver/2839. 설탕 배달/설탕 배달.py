f96416697623b1e8dff050ec295dd76ec9b143a0
Yoonho Cho <124619604+dev-yoonho@users.noreply.github.com>
2023-10-14 19:56:52 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+cnt = 0
+
+while n >= 0:
+    if n % 5 == 0:
+        cnt += n // 5
+        print(cnt)
+        break
+    n -= 3
+    cnt += 1
+else:
+    print(-1)
