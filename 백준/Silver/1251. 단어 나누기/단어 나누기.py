f59f201b8674fa3a25e5a762c0ebaa71a0d30004
Yoonho Cho <124619604+dev-yoonho@users.noreply.github.com>
2023-10-16 15:55:50 +0900

    [Silver V] Title: 단어 나누기, Time: 64 ms, Memory: 34028 KB -BaekjoonHub

---
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.py
@@ -0,0 +1,18 @@
+import sys
+from collections import deque
+
+s = list(sys.stdin.readline().rstrip())
+answer = []
+temp = []
+
+for i in range(1, len(s) - 1):
+    for j in range(i + 1, len(s)):
+        a = s[:i]
+        b = s[i:j]
+        c = s[j:]
+        a.reverse()
+        b.reverse()
+        c.reverse()
+        temp.append("".join(a + b + c))
+
+print(sorted(temp)[0])
