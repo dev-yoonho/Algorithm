09873807bdd795ca9323e54224bfaca298eb34c2
Yoonho Cho <124619604+dev-yoonho@users.noreply.github.com>
2023-10-26 20:48:17 +0900

    [Bronze I] Title: 뒤집힌 덧셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1357. 뒤집힌 덧셈/뒤집힌 덧셈.py
+++ 백준/Bronze/1357. 뒤집힌 덧셈/뒤집힌 덧셈.py
@@ -1,3 +1,1 @@
-import sys
-
-print(int(str(sum([int(i[::-1]) for i in list(map(str, sys.stdin.readline().split()))]))[::-1]))
+print(int(str(sum([int(i[::-1]) for i in list(map(str, input().split()))]))[::-1]))
