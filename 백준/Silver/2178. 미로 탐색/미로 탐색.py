3363b45673c585dbeeaf877fa6daebdf8558ed39
Yoonho Cho <124619604+dev-yoonho@users.noreply.github.com>
2023-11-04 20:22:12 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 34044 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,25 @@
+import sys
+from collections import deque
+
+n, m = map(int, sys.stdin.readline().split())
+graph = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(n)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    q = deque()
+    q.append((x, y))
+
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1:
+                q.append((nx, ny))
+                graph[nx][ny] = graph[x][y] + 1
+    return graph[n - 1][m - 1]
+
+print(bfs(0, 0))
