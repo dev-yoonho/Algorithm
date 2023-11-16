8b811e766ba02a8c8a462e3df34250548ecb2848
Yoonho Cho <124619604+dev-yoonho@users.noreply.github.com>
2023-11-17 03:49:13 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,4 @@
+import sys
+
+name = {'NLCS': 'North London Collegiate School', 'BHA': 'Branksome Hall Asia', 'KIS': 'Korea International School', 'SJA': 'St. Johnsbury Academy'}
+print(name[sys.stdin.readline().rstrip()])
