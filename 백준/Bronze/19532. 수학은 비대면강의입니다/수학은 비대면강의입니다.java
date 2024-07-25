fbd69e0c28723148d9feb72b9a092f25358da78d
Yoonho Cho <124619604+dev-yoonho@users.noreply.github.com>
2024-07-25 17:30:35 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 208 ms, Memory: 17144 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		int e = Integer.parseInt(st.nextToken());
+		int f = Integer.parseInt(st.nextToken());
+		
+		for (int i = -999;i < 1000;i++) {
+			for (int j = -999;j < 1000;j++) {
+				if ((a * i + b * j == c) && (d * i + e *j == f)) {
+					System.out.println(String.format("%d %d", i, j));
+					break;
+				}
+			}
+		}
+		br.close();
+	}
+}
