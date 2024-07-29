0bf79f166d04f0b1f5686d8ca456ad6b887abc4a
Yoonho Cho <124619604+dev-yoonho@users.noreply.github.com>
2024-07-29 16:49:27 +0900

    [D1] Title: 최대수 구하기, Time: 105 ms, Memory: 43,904 KB -BaekjoonHub

--- SWEA/D1/2068. 최대수 구하기/최대수 구하기.py
+++ SWEA/D1/2068. 최대수 구하기/최대수 구하기.py
@@ -1,4 +1,34 @@
-t = int(input())
-for i in range(t):
-    num_list = list(map(int, input().split()))
-    print(f"#{i+1} {max(num_list)}")
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		for (int i = 1;i < t + 1;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int[] numList = new int[10];
+			for (int j = 0;j < 10;j++) {
+				numList[j] = Integer.parseInt(st.nextToken());
+			}
+			System.out.println(String.format("#%d %d", i, getMaxNum(numList)));
+		}
+	}
+	
+	private static int getMaxNum(int[] arr) {
+		for (int i = arr.length - 1;i > 0;i--) {
+			for (int j = 0;j < i;j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j + 1];
+					arr[j + 1] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		return arr[arr.length - 1];
+	}
+}
