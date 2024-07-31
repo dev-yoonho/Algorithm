7b4c179010d8d6c42e65402a2f3d448bffa25eba
Yoonho Cho <124619604+dev-yoonho@users.noreply.github.com>
2024-07-31 15:47:53 +0900

    [Bronze IV] Title: $A + B - C$, Time: 160 ms, Memory: 16332 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        
+        bw.write(String.format("%d\n", a + b - c));
+        bw.write(String.format("%d\n", Integer.valueOf(a + "" + b) - c));
+        
+        bw.flush();
+        
+        br.close();
+        bw.close();
+	}
+}
