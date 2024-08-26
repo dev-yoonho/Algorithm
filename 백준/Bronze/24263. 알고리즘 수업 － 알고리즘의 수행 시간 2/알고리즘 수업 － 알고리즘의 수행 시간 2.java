9a5306138781b9a5dde31275679a84a8ac5a219b
Yoonho Cho <124619604+dev-yoonho@users.noreply.github.com>
2024-08-26 15:48:43 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 164 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		// 입력의 크기 n
+		int n = sc.nextInt();
+		
+		System.out.println(n);
+		System.out.println(1);
+		
+		sc.close();
+	}
+}
