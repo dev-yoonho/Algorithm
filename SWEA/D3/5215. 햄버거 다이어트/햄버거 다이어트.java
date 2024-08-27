d2c360afc03563126786ae47464b3e0215854137
Yoonho Cho <124619604+dev-yoonho@users.noreply.github.com>
2024-08-27 13:10:17 +0900

    [D3] Title: 햄버거 다이어트, Time: 1,462 ms, Memory: 24,692 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,35 +1,58 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
-class Solution {
-	static int T;
+public class Solution { // 부분집합 문제
+
+	static int[][] ingredient; // 재료배열 - 맛 점수, 칼로리
 	static int N, L;
-	static int[][] items;
-	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		T = sc.nextInt();
-		for (int tc = 1;tc <= T;tc++) {
-			N = sc.nextInt();
-			L = sc.nextInt();
+	
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		// 테스트 케이스의 개수 t
+		int t = Integer.parseInt(br.readLine());
+		
+		for (int tc = 1;tc <= t;tc++) {
+			// 재료의 수 N, 제한 칼로리 L
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken()); 
+			L = Integer.parseInt(st.nextToken());
 			
-			items = new int[N + 1][2];
-			for (int i = 1;i <= N;i++) {
-				items[i][1] = sc.nextInt();
-				items[i][0] = sc.nextInt();
+			// 재료 배열 준비
+			ingredient = new int[N][2];
+			for (int i = 0;i < N;i++) {
+				st = new StringTokenizer(br.readLine());
+				ingredient[i][0] = Integer.parseInt(st.nextToken());
+				ingredient[i][1] = Integer.parseInt(st.nextToken());
 			}
+			// 칼로리 합 cal, 점수 합 scr
+			int cal, scr;
 			
-			int[][] dp = new int[N + 1][L + 1];
+			int result = 0;
 			
-			for (int i = 1;i <= N;i++) {
-				for (int j = 1;j <= L;j++) {
-					if (items[i][0] > j) {
-						dp[i][j] = dp[i-1][j];
-					} else {
-						dp[i][j] = Math.max(dp[i-1][j], dp[i - 1][j - items[i][0]] + items[i][1]);
+			for (int i = 0;i < (1 << N);i++) {
+				cal = 0;
+				scr = 0;
+				for (int j = 0;j < N;j++) {
+					if ((i & (1 << j)) > 0) {
+						cal += ingredient[j][1];
+						scr += ingredient[j][0];
 					}
 				}
+				result = cal <= L && scr > result ? scr:result;
 			}
-			System.out.printf("#%d %d\n", tc, dp[N][L]);
+			bw.write(String.format("#%d %d\n", tc, result));
 		}
-		sc.close();
+		bw.flush();
+		
+		br.close();
+		bw.close();
 	}
 }
