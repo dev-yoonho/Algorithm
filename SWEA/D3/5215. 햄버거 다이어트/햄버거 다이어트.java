0df67f5bb3f27ebd0d6f24ebd1961a24f72a595d
Yoonho Cho <124619604+dev-yoonho@users.noreply.github.com>
2024-09-04 13:51:16 +0900

    [D3] Title: 햄버거 다이어트, Time: 236 ms, Memory: 24,044 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,58 +1,48 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
-public class Solution { // 부분집합 문제
+public class Solution {
+    static int N, L; // 재료의 수와 제한 칼로리
+    static int[] cals;
+    static int[] scores;
+    static int ans;
 
-	static int[][] ingredient; // 재료배열 - 맛 점수, 칼로리
-	static int N, L;
-	
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-		
-		// 테스트 케이스의 개수 t
-		int t = Integer.parseInt(br.readLine());
-		
-		for (int tc = 1;tc <= t;tc++) {
-			// 재료의 수 N, 제한 칼로리 L
-			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken()); 
-			L = Integer.parseInt(st.nextToken());
-			
-			// 재료 배열 준비
-			ingredient = new int[N][2];
-			for (int i = 0;i < N;i++) {
-				st = new StringTokenizer(br.readLine());
-				ingredient[i][0] = Integer.parseInt(st.nextToken());
-				ingredient[i][1] = Integer.parseInt(st.nextToken());
-			}
-			// 칼로리 합 cal, 점수 합 scr
-			int cal, scr;
-			
-			int result = 0;
-			
-			for (int i = 0;i < (1 << N);i++) {
-				cal = 0;
-				scr = 0;
-				for (int j = 0;j < N;j++) {
-					if ((i & (1 << j)) > 0) {
-						cal += ingredient[j][1];
-						scr += ingredient[j][0];
-					}
-				}
-				result = cal <= L && scr > result ? scr:result;
-			}
-			bw.write(String.format("#%d %d\n", tc, result));
-		}
-		bw.flush();
-		
-		br.close();
-		bw.close();
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 테스트 케이스의 개수 t
+        int t = sc.nextInt();
+
+        for (int tc = 1; tc <= t; tc++) {
+            N = sc.nextInt();
+            L = sc.nextInt();
+
+            scores = new int[N];
+            cals = new int[N];
+
+            for (int i = 0; i < N; i++) {
+                scores[i] = sc.nextInt();
+                cals[i] = sc.nextInt();
+            } // input 끝
+            ans = 0;
+
+            makeBurger(0, 0, 0);
+            System.out.println("#" + tc + " " + ans);
+        } // tc
+    } // main
+
+    // 중간 결과를 들고 다니겠다!
+    static void makeBurger(int idx, int sumScore, int sumCal) {
+        // 기저조건: 모든 재료를 고려한 경우
+        if (sumCal > L) return;
+        if (idx == N) {
+            ans = Math.max(sumScore, ans);
+            return;
+        }
+
+        // 재료를 선택하는 경우
+        makeBurger(idx + 1, sumScore + scores[idx], sumCal + cals[idx]);
+
+        // 재료를 선택하지 않는 경우
+        makeBurger(idx + 1, sumScore, sumCal);
+    }
 }
