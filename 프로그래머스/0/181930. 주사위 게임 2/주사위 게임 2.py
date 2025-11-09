a0d508976ef618b49c696f54794ab31ff460be2f
Yoonho Cho <124619604+dev-yoonho@users.noreply.github.com>
2025-11-09 21:37:24 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.00 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -0,0 +1,2 @@
+def solution(a, b, c):
+    return a + b + c if (a != b) & (b != c) & (c != a) else (a + b + c) * (a ** 2 + b ** 2 + c ** 2) * (a ** 3 + b ** 3 + c ** 3) if a == b == c  else (a + b + c) * (a ** 2 + b ** 2 + c ** 2)
