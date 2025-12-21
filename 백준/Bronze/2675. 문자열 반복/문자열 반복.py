623cf48462ccb7941308e9be564e945c8ee35f61
Yoonho Cho <124619604+dev-yoonho@users.noreply.github.com>
2025-12-22 00:26:44 +0900

    [Bronze II] Title: 문자열 반복, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for i in range(t):
+    r, s = input().split()
+    r = int(r)
+
+    for i in range(len(s)):
+        print(s[i] * r, end="")
+    print()
