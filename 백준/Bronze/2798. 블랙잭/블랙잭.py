fc64c674078f30bfb89316afcc3005ea47f761ea
Yoonho Cho <124619604+dev-yoonho@users.noreply.github.com>
2025-12-23 00:05:12 +0900

    [Bronze II] Title: 블랙잭, Time: 52 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,12 +1,20 @@
 import sys
-from itertools import combinations
+input = sys.stdin.readline
 
-n, m = map(int, sys.stdin.readline().split())
-card = list(map(int, sys.stdin.readline().split()))
+n, m = map(int, input().split())
+n_list = list(map(int, input().split()))
 
-temp = [sum(i) for i in list(combinations(card, 3))]
-temp.sort(reverse = True)
-for i in temp:
-    if i <= m:
-        print(i)
-        break
+def win_case(n_list):
+    result = 0
+    length = len(n_list)
+    
+    for i in range(length):
+        for j in range(i+1, length):
+            for k in range(j+1, length):
+                cur_sum = n_list[i]+n_list[j]+n_list[k]
+                
+                if cur_sum <= m:
+                    result = max(result, cur_sum)
+    return result
+
+print(win_case(n_list))
