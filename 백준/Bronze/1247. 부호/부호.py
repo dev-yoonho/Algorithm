aa9e22111ddb6711c98bf4d1831adfb50dc22da0
Yoonho Cho <124619604+dev-yoonho@users.noreply.github.com>
2026-01-28 00:44:52 +0900

    [Bronze III] Title: 부호, Time: 304 ms, Memory: 112160 KB -BaekjoonHub

---
+++ 백준/Bronze/1247. 부호/부호.py
@@ -0,0 +1,38 @@
+n1 = int(input())
+
+s = 0 
+for i in range(n1):
+    s += int(input())
+
+if s == 0:
+    print("0")
+elif s > 0:
+    print("+")
+else:
+    print("-")
+
+n2 = int(input())
+
+s = 0 
+for i in range(n2):
+    s += int(input())
+
+if s == 0:
+    print("0")
+elif s > 0:
+    print("+")
+else:
+    print("-")
+
+n3 = int(input())
+
+s = 0 
+for i in range(n3):
+    s += int(input())
+
+if s == 0:
+    print("0")
+elif s > 0:
+    print("+")
+else:
+    print("-")
