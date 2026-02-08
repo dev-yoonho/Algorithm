d7439d44ab3c1b1536c4664d16fa3231256a94bd
Yoonho Cho <124619604+dev-yoonho@users.noreply.github.com>
2026-02-09 01:30:43 +0900

    [Bronze IV] Title: Covid-19, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/20673. Covid－19/Covid－19.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+p = int(input())
+q = int(input())
+
+if p <= 50 and q <= 10:
+    print("White")
+elif q > 30:
+    print("Red")
+else:
+    print("Yellow")
